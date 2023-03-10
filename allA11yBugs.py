8302ec4926922f50d32525b719e9ec977510cb97
James Teh <jamie@jantrid.net>
2023-03-10 10:30:37 +1000

    allA11yBugs: Put enhancement/task in the severity column. Sort enhancements after defects and tasks after enhancements.

--- allA11yBugs.py
+++ allA11yBugs.py
@@ -4,6 +4,7 @@ List all bugs related to accessibility.
 	with the access keyword.
 - Map access-s ratings to severity ratings.
 - Map old severity values (major and normal) to new severity values (s2 and s3).
+- Present enhancement/task as severity, sorting after defects.
 - Sort by severity (highest first), product and component (alphabetical), then
 	creation time (most recent first).
 - Output as a tab separated table.
@@ -13,7 +14,7 @@ import re
 import urllib.parse
 import requests
 
-QUERY_PREFIX = "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,product,component,severity,whiteboard&limit=0&quicksearch="
+QUERY_PREFIX = "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,product,component,severity,type,whiteboard&limit=0&quicksearch="
 # Quick search queries. All queries will be merged into the output list.
 QUERIES = [
 	"component:disability -product:thunderbird",
@@ -30,6 +31,12 @@ def sortKey(bug):
 	if severity in ("", "--", "n/a"):
 		# No/invalid severity. Sort last.
 		severity = "s5"
+	elif severity == "enhancement":
+		# Enhancements go after all defects.
+		severity = "s6"
+	elif severity == "task":
+		# Tasks go after enhancements.
+		severity = "s7"
 	bugId = int(bug["id"])
 	# We want to sort descending on bug id to get most recent bugs first, so
 	# multiply by -1.
@@ -44,10 +51,15 @@ def fullQuery():
 		bugs.extend(getBugs(query))
 	for bug in bugs:
 		severity = bug["severity"]
+		bugType = bug["type"]
 		m = RE_ACCESS_S.search(bug["whiteboard"])
 		if m:
 			# Map access-s whiteboard tag to severity.
 			severity = m.group(1)
+		elif bugType != "defect":
+			# Severity is irrelevant for enhancements/tasks, so put that in the
+			# severity field.
+			severity = bugType
 		else:
 			severity = severity.lower()
 			# Map some old severity values to new ones.
